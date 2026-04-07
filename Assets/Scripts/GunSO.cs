8a6eb62a0e6d2a1931792877ded4dce716d5fdb7
FaheemFHM <147944491+FaheemFHM@users.noreply.github.com>
2026-04-07 18:58:06 +0100

    Initial Gun Commit

---
+++ Assets/Scripts/GunSO.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewGun", menuName = "Gun")]
+public class GunSO : ScriptableObject
+{
+    public Sprite sprite;
+    public Sprite crosshair;
+
+    public Vector2 firePoint;
+    [Range(0.1f, 2f)] public float fireRate = 0.333f;
+
+    public bool isAuto = true;
+
+    [Range(1, 100)] public int ammoCost = 1;
+
+    [Range(1, 5)] public int burstCount = 1;
+    [Range(1, 5)] public int spreadCount = 1;
+    [Range(0f, 45f)] public float spreadAngle = 0f;
+
+    public GameObject bulletPrefab;
+    [Range(1f, 20f)] public float bulletSpeed = 10f;
+    [Range(0f, 100f)] public float damage = 1f;
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,24 +8,60 @@ public class PlayerMove : MonoBehaviour
     [Header("Movement")]
     [SerializeField, Range(0f, 10f)] private float moveSpeed = 4f;
     [SerializeField, Range(0f, 10f)] private float sprintSpeed = 6f;
+    [SerializeField, Range(0f, 1f)] private float verticalMoveDamping = 0.9f;
     private Vector2 moveInput;
     private Vector2 aimInput;
+    private Vector2 prevAimInput;
+
+    [Header("Shooting")]
+    [SerializeField] private Transform gunPivot; // does the spin effect
+    private Transform gunHolder; // does the aiming rotation
+    private Transform gunTransform; // does the recoil translation
+
+    [SerializeField, Range(0f, 360f)] private float gunSpinSpeed = 135f;
+    [SerializeField] private bool gunSpinClockwise = true;
 
     private void Awake()
     {
+        // components
         inputs = GetComponent<InputManager>();
         rb = GetComponent<Rigidbody2D>();
+
+        // transforms
+        gunHolder = gunPivot.GetChild(0);
+        gunTransform = gunHolder.GetChild(0);
     }
 
     private void Update()
     {
-        moveInput = inputs.MoveInput;
-        aimInput = inputs.AimInput;
+        // get inputs
+        moveInput = inputs.MoveInput.normalized;
+        aimInput = inputs.AimInput.normalized;
+
+        // use move as aim if aim is none
+        if (aimInput.sqrMagnitude > 0.01f) prevAimInput = aimInput;
+        else if (moveInput.sqrMagnitude > 0.01f) prevAimInput = moveInput;
+        aimInput = prevAimInput;
+
+        // spin gun
+        int spinDir = (gunSpinClockwise ? -1 : 1);
+        gunPivot.Rotate(0, 0, gunSpinSpeed * spinDir * Time.deltaTime);
+
+        // aim gun
+        float angle = Mathf.Atan2(aimInput.y, aimInput.x) * Mathf.Rad2Deg;
+        gunHolder.rotation = Quaternion.Euler(0, 0, angle);
+
+        // gun direction
+        Vector3 scale = gunTransform.localScale;
+        scale.y = aimInput.x < 0 ? -Mathf.Abs(scale.y) : Mathf.Abs(scale.y);
+        gunTransform.localScale = scale;
     }
 
     private void FixedUpdate()
     {
+        // move
         float speed = inputs.IsSprinting ? sprintSpeed : moveSpeed;
-        rb.linearVelocity = moveInput * speed;
+        Vector2 vel = new Vector2(moveInput.x, moveInput.y * verticalMoveDamping);
+        rb.linearVelocity = vel * speed;
     }
 }
