a293636612bff13f6779745d8328ac09dd443754
FaheemFHM <147944491+FaheemFHM@users.noreply.github.com>
2026-04-07 21:09:15 +0100

    Gun Shooting

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,6 +9,7 @@ public class PlayerMove : MonoBehaviour
     [SerializeField, Range(0f, 10f)] private float moveSpeed = 4f;
     [SerializeField, Range(0f, 10f)] private float sprintSpeed = 6f;
     [SerializeField, Range(0f, 1f)] private float verticalMoveDamping = 0.9f;
+    [SerializeField] private Transform spriteTransform;
     private Vector2 moveInput;
     private Vector2 moveNorm;
     private Vector2 aimInput;
@@ -18,32 +19,49 @@ public class PlayerMove : MonoBehaviour
     private int dir = 1;
 
     [Header("Shooting")]
+    [SerializeField] private Transform firePoint;
     [SerializeField] private Transform gunPivot; // does the spin effect
     [SerializeField] private Transform gunHolder; // does the aiming rotation
     private Transform gunTransform; // does the recoil translation
     private Vector2 gunOffset;
 
+    [SerializeField] private GunSO[] guns;
+    private SpriteRenderer gunRend;
+    private int gunsIndex = 0;
+    private GunSO gun;
+
+    [SerializeField] private Transform bulletFolder;
+
+    private float fireCooldown;
+    private bool hasFired;
+
+    [Header("Crosshair")]
     [SerializeField, Range(0f, 360f)] private float gunSpinSpeed = 135f;
     [SerializeField] private bool gunSpinClockwise = true;
-
     [SerializeField] private Transform crosshair;
+    [SerializeField, Range(0f, 2f)] private float crosshairDistMin = 0.5f;
+    [SerializeField, Range(1f, 10f)] private float crosshairDistMax = 3f;
     private SpriteRenderer crossRend;
 
-    [SerializeField] private float crosshairDistMin = 0.5f;
-    [SerializeField] private float crosshairDistMax = 3f;
-
-    [SerializeField] private Transform sprite;
-
-    private void Awake()
+    private void Start()
     {
         // components
         inputs = GetComponent<InputManager>();
         rb = GetComponent<Rigidbody2D>();
 
+        // subscriptions
+        inputs.onSwitch += NextGun;
+        inputs.onPrimary += PressFire;
+
+        // gun
         gunTransform = gunHolder.GetChild(0);
+        gunRend = gunTransform.GetComponentInChildren<SpriteRenderer>();
         gunOffset = Vector2.up * gunHolder.localPosition.y;
 
         crossRend = crosshair.GetComponent<SpriteRenderer>();
+
+        gunsIndex = guns.Length - 1;
+        NextGun();
     }
 
     private void Update()
@@ -64,7 +82,7 @@ public class PlayerMove : MonoBehaviour
         else if (myNorm.x > 0.01f) dir = 1;
 
         // flip character
-        sprite.localScale = new Vector3(dir, 1, 1);
+        spriteTransform.localScale = new Vector3(dir, 1, 1);
 
         // spin gun
         int spinDir = (gunSpinClockwise ? -1 : 1);
@@ -91,6 +109,22 @@ public class PlayerMove : MonoBehaviour
         crossRend.color = c;
 
         crosshair.localScale = Vector2.one * op;
+
+        // shooting
+        fireCooldown -= Time.deltaTime;
+
+        if (inputs.IsPrimary)
+        {
+            if (fireCooldown < 0f)
+            {
+                if ((!gun.isAuto && !hasFired) || gun.isAuto)
+                {
+                    Shoot();
+                    fireCooldown = gun.fireRate;
+                    hasFired = true;
+                }
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -99,5 +133,33 @@ public class PlayerMove : MonoBehaviour
         float speed = inputs.IsSprinting ? sprintSpeed : moveSpeed;
         Vector2 vel = new Vector2(moveNorm.x, moveNorm.y * verticalMoveDamping);
         rb.linearVelocity = vel * speed;
+    }
+
+    void Shoot()
+    {
+        GameObject b = Instantiate(gun.bulletPrefab, firePoint.position, firePoint.rotation, bulletFolder);
+        b.GetComponent<Rigidbody2D>().linearVelocity = myNorm * gun.bulletSpeed;
+        Destroy(b, 3f);
+    }
+
+    void NextGun(bool isPressing = true)
+    {
+        if (!isPressing) return;
+
+        gunsIndex = (gunsIndex + 1 + guns.Length) % guns.Length;
+        SetGun(guns[gunsIndex]);
+    }
+
+    void SetGun(GunSO newGun)
+    {
+        gun = newGun;
+        gunRend.sprite = gun.sprite;
+        firePoint.localPosition = gun.firePoint;
+        crossRend.sprite = gun.crosshair;
+    }
+
+    void PressFire(bool isPressing)
+    {
+        if (!isPressing) hasFired = false;
     }
 }
