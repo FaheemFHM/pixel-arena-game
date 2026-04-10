ae5c74c65705a2b972cb1e12f11dde8cc9a05377
FaheemFHM <147944491+FaheemFHM@users.noreply.github.com>
2026-04-10 20:30:53 +0100

    Merge Prep

--- Assets/Scripts/MinimapMarker.cs
+++ Assets/Scripts/MinimapMarker.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 
-[System.Serializable]
-public class MinimapMarker
+public class MinimapMarker : MonoBehaviour
 {
     public MinimapMarkerDefinitionSO def;
     public Vector2 pos;
     public RectTransform rt;
     public CanvasGroup img;
+
+    private void Awake()
+    {
+        rt= GetComponent<RectTransform>();
+        img= GetComponent<CanvasGroup>();
+    }
 }
